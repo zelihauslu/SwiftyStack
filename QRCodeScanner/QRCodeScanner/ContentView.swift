974aae6ddd5000cd6a234c314094b343865879aa
zelihauslu <zelusluz@gmail.com>
2023-03-04 18:43:22 +0300

    Edited scanner sheet

--- QRCodeScanner/QRCodeScanner/ContentView.swift
+++ QRCodeScanner/QRCodeScanner/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan a QR code to get started."
+    
+    var scannerSheet : some View{
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing : 10){
+            Text(scannedCode)
+            
+            Button("Scan QR code"){
+                self.isPresentingScanner = true
+                
+            }.sheet(isPresented: $isPresentingScanner){
+                self.scannerSheet
+            }
+        }
     }
 }
 
