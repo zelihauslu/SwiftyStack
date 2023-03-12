59b952c490449ea8031e3dbf4829b1e1f70633c7
zelihauslu <zelusluz@gmail.com>
2023-03-12 21:27:11 +0300

    Encoded data to JSON

--- JSONParsing/JSONParsing/Decodable.playground/Contents.swift
+++ JSONParsing/JSONParsing/Decodable.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+//DECODABLE
+
 struct Customer : Decodable{
     var firstName : String
     var lastName : String
@@ -37,3 +39,36 @@ let json = """
 let customer = try! JSONDecoder().decode(Customer.self, from: json)
     print(customer)
 
+
+//ENCODABLE
+//---------------------------------------------------------------------------------------------
+
+struct  Person : Codable{
+    var firstName : String
+    var lastName : String
+    var age : Int
+}
+
+/*
+private enum CodingKeys : String, CodingKey {
+    case firstName
+    case lastName
+    case age
+}
+
+func encode(to encoder :Encoder) throws {
+    
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(self.firstName, forKey: .firstName)
+    try container.encode(self.lastName, forKey: .lastName)
+    try container.encode(self.age, forKey: .age)
+} */
+
+
+let person = Person(firstName: "John", lastName: "Doe", age: 45)
+
+let encodedCustomerJSON = try! JSONEncoder().encode(person)
+
+print(encodedCustomerJSON)
+
+print(String(data: encodedCustomerJSON, encoding: .utf8)!)
