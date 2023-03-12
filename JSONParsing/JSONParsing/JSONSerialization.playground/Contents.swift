46d0f9ef2224ae9fb3a17ed60901aab1b3e923f9
zelihauslu <zelusluz@gmail.com>
2023-03-12 18:20:17 +0300

    Added model

--- JSONParsing/JSONParsing/JSONSerialization.playground/Contents.swift
+++ JSONParsing/JSONParsing/JSONSerialization.playground/Contents.swift
@@ -1,6 +1,46 @@
 import UIKit
 import Foundation
 
+///model
+
+struct Customer{
+    var firstName : String
+    var lastName : String
+}
+
+struct Example{
+    var firstName : String
+    var lastName : String
+}
+
+extension Customer{
+    
+    init?(dictionary : [String : Any]){
+        
+        guard let firstName = dictionary["firstName"] as? String,
+              let lastName = dictionary["lastName"] as? String else {
+            return nil
+        }
+        
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+extension Example{
+    init?(dictionary : [String : Any]){
+        
+        guard let firstName = dictionary["firstName"] as? String,
+              let lastName = dictionary["lastName"] as? String else {
+            return nil
+        }
+        
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+}
+
+
 /// parse json
 
 let json = """
@@ -12,8 +52,16 @@ let json = """
     .data(using: .utf8)!
 
 do{
-    if let jsonData = try JSONSerialization.jsonObject(with: json, options: .fragmentsAllowed) as? [String: Any]{
-        jsonData["firstName"]
+    if let dictionary = try JSONSerialization.jsonObject(with: json, options: .fragmentsAllowed) as? [String: Any]{
+        dictionary["firstName"]
+        
+        if let customer = Customer(dictionary: dictionary){
+            print(customer)
+        }
+        
+        if let ex = Example(dictionary: dictionary){
+            print(ex)
+        }
     }
-
+    
 }
