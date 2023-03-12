cfa988afcd10b9fc3a9f8429761a7ddd8edc9073
zelihauslu <zelusluz@gmail.com>
2023-03-12 16:10:46 +0300

    initial commit

---
+++ JSONParsing/JSONParsing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSONParsing
+//
+//  Created by Zeliha Uslu on 12.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSONParsing/JSONParsing/JSONParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParsingApp.swift
+//  JSONParsing
+//
+//  Created by Zeliha Uslu on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct JSONParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
