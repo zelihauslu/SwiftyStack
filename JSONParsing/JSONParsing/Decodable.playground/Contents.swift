b76dbd0331a9ac95802e415b89be29a0672c0935
zelihauslu <zelusluz@gmail.com>
2023-03-12 20:05:21 +0300

    Decoded JSON into model

--- JSONParsing/JSONParsing/Decodable.playground/Contents.swift
+++ JSONParsing/JSONParsing/Decodable.playground/Contents.swift
@@ -1,2 +1,39 @@
 import UIKit
 
+struct Customer : Decodable{
+    var firstName : String
+    var lastName : String
+    var age : Int
+    
+    /// decodable doing those stuffs:
+    
+//    private enum CodingKeys : String, CodingKey{
+//        case firstName
+//        case lastName
+//        case age
+//    }
+//
+//    init(from decoder : Decoder) throws{
+//
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//
+//        self.firstName = try container.decode(String.self, forKey: .firstName)
+//        self.lastName = try container.decode(String.self, forKey: .lastName)
+//        self.age = try container.decode(Int.self, forKey: .age)
+//    }
+}
+
+
+let json = """
+{
+"firstName" : "Zeliha",
+"lastName" : "Uslu",
+"age" : 20
+}
+"""
+    .data(using: .utf8)!
+
+
+let customer = try! JSONDecoder().decode(Customer.self, from: json)
+    print(customer)
+
