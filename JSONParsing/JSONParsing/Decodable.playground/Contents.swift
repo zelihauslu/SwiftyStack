0c8a615c72a4ba2f772d092434d95f6948a2d445
zelihauslu <zelusluz@gmail.com>
2023-03-13 13:44:45 +0300

    Decoding JSON array

--- JSONParsing/JSONParsing/Decodable.playground/Contents.swift
+++ JSONParsing/JSONParsing/Decodable.playground/Contents.swift
@@ -72,3 +72,58 @@ let encodedCustomerJSON = try! JSONEncoder().encode(person)
 print(encodedCustomerJSON)
 
 print(String(data: encodedCustomerJSON, encoding: .utf8)!)
+
+
+
+
+
+
+// Decoding JSON ARRAY
+//---------------------------------------------------------------------------------------------
+
+
+struct Place : Decodable {
+    
+    var name :String
+    var latitude :Double
+    var longitude :Double
+    var visited :Bool
+}
+
+let json2 = """
+
+[
+{
+"name" : "Costa Rica",
+"latitude" : 23.45,
+"longitude" : 45.23,
+"visited" : true
+},
+{
+"name" : "Puerto Rico",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : true
+},
+{
+"name" : "Mexico City",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : true
+},
+{
+"name" : "Iceland",
+"latitude" : 23,
+"longitude" : 45,
+"visited" : false
+}
+]
+
+""".data(using: .utf8)!
+
+let places = try! JSONDecoder().decode([Place].self, from: json2) //array
+//let customer = try! JSONDecoder().decode(Customer.self, from: json)
+places[0].name
+places[1].name
+places[2].name
+print(places)
