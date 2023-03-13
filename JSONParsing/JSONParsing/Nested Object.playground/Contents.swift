a19963dab6d2d8ac386d7643977dc1427a657904
zelihauslu <zelusluz@gmail.com>
2023-03-13 14:15:08 +0300

    Decoding a Nested Object

--- JSONParsing/JSONParsing/Nested Object.playground/Contents.swift
+++ JSONParsing/JSONParsing/Nested Object.playground/Contents.swift
@@ -1,2 +1,55 @@
 import UIKit
 
+struct Geo : Decodable {
+    var latitude :Double
+    var longitude :Double
+}
+
+struct Address : Decodable {
+    var street :String
+    var city :String
+    var state :String
+    var geo :Geo
+}
+
+
+struct Customer : Decodable {
+    var firstName :String
+    var lastName :String
+    var address :Address
+}
+
+struct CustomersResponse :Decodable {
+    var customers :[Customer]
+}
+
+
+let json = """
+
+{
+    "customers":[
+        {
+            "firstName" : "John",
+            "lastName" : "Doe",
+            "address" : {
+                "street" : "1200 Richmond Ave",
+                "city" : "Houston",
+                "state" : "TX",
+                "geo" : {
+                    "latitude" : 34.56,
+                    "longitude" : 35.65
+                }
+            }
+        }
+    
+    ]
+
+}
+
+""".data(using: .utf8)!
+
+let customersResponse = try! JSONDecoder().decode(CustomersResponse.self, from: json)
+print(customersResponse)
+print(customersResponse.customers[0].address.geo.latitude)
+
+
