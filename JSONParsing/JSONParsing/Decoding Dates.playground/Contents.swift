8fa2a3eb99641a2a578826ddf47ce670c5ea3aff
zelihauslu <zelusluz@gmail.com>
2023-03-13 15:55:41 +0300

    Decoding Dates

--- JSONParsing/JSONParsing/Decoding Dates.playground/Contents.swift
+++ JSONParsing/JSONParsing/Decoding Dates.playground/Contents.swift
@@ -1,3 +1,69 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// reference - useyourloaf.com
+extension DateFormatter {
+    static let iso8601Full :DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd/yyy"
+        return formatter
+    }()
+}
+
+struct Geo : Decodable {
+    var latitude :Double
+    var longitude :Double
+}
+
+struct Address : Decodable {
+    var street :String
+    var city :String
+    var state :String
+    var geo :Geo
+}
+
+
+struct Customer : Decodable {
+    var firstName :String
+    var lastName :String
+    var address :Address
+    var dateCreated :Date
+}
+
+struct CustomersResponse :Decodable {
+    var customers :[Customer]
+}
+
+
+let json = """
+
+{
+    "customers":[
+        {
+            "firstName" : "John",
+            "lastName" : "Doe",
+            "dateCreated" : "05/09/2018",
+            "address" : {
+                "street" : "1200 Richmond Ave",
+                "city" : "Houston",
+                "state" : "TX",
+                "geo" : {
+                    "latitude" : 34.56,
+                    "longitude" : 35.65
+                }
+            }
+        }
+    
+    ]
+
+}
+
+""".data(using: .utf8)!
+
+let decoder = JSONDecoder()
+decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+
+
+let customerResponse = try! decoder.decode(CustomersResponse.self, from: json)
+print(customerResponse)
+
+
