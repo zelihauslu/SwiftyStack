4d199e11c7b1e2f259a8eed3c492c4d2ff0f411b
zelihauslu <zelusluz@gmail.com>
2023-03-14 13:21:35 +0300

    Handling Property Names

--- JSONParsing/JSONParsing/Handling Property Names.playground/Contents.swift
+++ JSONParsing/JSONParsing/Handling Property Names.playground/Contents.swift
@@ -1,2 +1,27 @@
 import UIKit
 
+struct Customer : Decodable {
+    var firstName :String
+    var lastName :String
+    var age :Int
+}
+
+let json = """
+
+{
+    "first_name" : "John",
+    "last_name" : "Doe",
+    "age" : 34
+}
+
+""".data(using: .utf8)!
+
+let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+let customer = try! decoder.decode(Customer.self, from: json)
+
+print(customer)
+
+
+
