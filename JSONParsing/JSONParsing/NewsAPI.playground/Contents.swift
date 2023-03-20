bf2662a852ce9bc53f34502d106f76beca4197fa
zelihauslu <zelusluz@gmail.com>
2023-03-20 18:10:32 +0300

    Created NewsAPI

---
+++ JSONParsing/JSONParsing/NewsAPI.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let json = """
+"articles": [
+-{
+-"source": {
+"id": null,
+"name": "Electrek"
+},
+"author": "Peter Johnson",
+"title": "",
+"description": "",
+"url": "",
+"urlToImage": "",
+"publishedAt": "2023-03-20T14:31:08Z",
+"content": ""
+}
+""".data(using: .utf8)
+
+
