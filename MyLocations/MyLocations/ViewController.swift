eac8ec8b4ef35dc14b6a68d82fb697b6e6f16415
zelihauslu <zelusluz@gmail.com>
2023-03-24 17:42:26 +0300

    Initial Commit

---
+++ MyLocations/MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Zeliha Uslu on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
