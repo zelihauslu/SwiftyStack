cb853a2e12531cb6d21bdca9cb5961d93dae22f3
zelihauslu <zelusluz@gmail.com>
2023-03-28 21:22:14 +0300

    Main Source Code Update

---
+++ InstagramCloneFirebase/InstagramCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Zeliha Uslu on 28.03.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
