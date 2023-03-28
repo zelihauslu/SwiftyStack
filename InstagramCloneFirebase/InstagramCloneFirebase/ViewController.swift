e4ab01a543ec0dbf8e2a438faaba1cb8a3e57122
zelihauslu <zelusluz@gmail.com>
2023-03-28 21:03:16 +0300

    Module Update

--- InstagramCloneFirebase/InstagramCloneFirebase/ViewController.swift
+++ InstagramCloneFirebase/InstagramCloneFirebase/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func logInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        performSegue(withIdentifier: "ToFeedVC", sender: nil)
 
+    }
 }
 
