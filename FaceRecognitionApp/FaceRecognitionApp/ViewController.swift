9a9827df31c01fdca9b90348bd0ca46013463ccd
zelihauslu <zelusluz@gmail.com>
2023-04-04 22:30:05 +0300

    Initial Commit

---
+++ FaceRecognitionApp/FaceRecognitionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognitionApp
+//
+//  Created by Zeliha Uslu on 4.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
