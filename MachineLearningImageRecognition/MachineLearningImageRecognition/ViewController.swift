1e7c58706afed831c88f9e3c8bb5e43007f8f1c9
zelihauslu <zelusluz@gmail.com>
2023-04-05 18:26:44 +0300

    UI Edited

--- MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeClicked(_ sender: Any) {
+    }
+    
 }
 
